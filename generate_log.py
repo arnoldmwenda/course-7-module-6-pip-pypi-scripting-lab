a403113f87165206ebf1565bdf6552cbb359ccd8
arnoldmwenda <arnoldmwenda668@gmail.com>
2026-06-15 13:45:48 +0300

    Second Commit

---
+++ generate_log.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import os
+
+def generate_log(log_entries):
+    if not isinstance(log_entries, list):
+        raise ValueError("Input must be a list")
+
+    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
+
+    with open(filename, "w") as file:
+        for entry in log_entries:
+            file.write(f"{entry}\n")
+
+    return filename
